ae7d7974fb9223ce0d114feaeb71bedd8ac3a9a7
Teboho Thapeli <tm.thapeli@gmail.com>
2022-10-05 00:03:59 +0200

    removed genius api access token from lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -2,7 +2,7 @@ import lyricsgenius
 
 # generate an api key and paste it
 # https://genius.com/api-clients
-genius = lyricsgenius.Genius("MBo4sibIPGj6y_Mh6Cxe9ATsr4nLI7clZfsnyDUbr4CGFsX3_EtRNWMYS01wSmLM")
+genius = lyricsgenius.Genius("")
 
 
 def save_lyrics(songs, artist_name, album_name):
